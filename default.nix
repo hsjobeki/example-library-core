2d1374f44818c9fb4c989849a9c5ef2be829d748
Johannes Kirschbauer <johannes.kirschbauer@secunet.com>
2022-10-04 13:10:13 +0200

    dream2nix init

--- default.nix
+++
@@ -1,3 +0,0 @@
-{
-  inherit (import ./nix/release.nix) mui-theme;
-} 
--- nix/release.nix
+++
@@ -1,7 +0,0 @@
-let
-  sources = import ./sources.nix;
-  pkgs = import sources.nixpkgs { overlays = [ (import ./overlay.nix) ]; };
-in
-{
-  inherit (pkgs.secunet) mui-theme;
-}
